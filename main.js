c108a45019066ce0223ddcd62068a725660ea7c7
Myron Anthony <codehearted12@gmail.com>
2017-08-01 16:46:36 -0400

    Etsy

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 // you have access to the `data` variable that contains the Etsy Store data.
 // Open your `index.html` page in your browser and you can verify that the following
 // is properly working. The `data` variable is an array with 25 items in it
-  console.log(data);
+console.log(data);
 
 
 
-  function question1() {
+function question1() {
   // Answer:
   let num = 0;
   for (let i = 0; i < data.length; i++) {
@@ -19,24 +19,24 @@
 
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-  function question2 () {
+function question2() {
   // Answer:
   data.forEach(function(i) {
-  if(i.price >= 14 && i.price <= 18) {
-  console.log (i.title);
-  }
+    if (i.price >= 14 && i.price <= 18) {
+      console.log(i.title);
+    }
   });
 }
 
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
-  function question3 () {
+function question3() {
   // Answer:
   data.forEach(function(element) {
-  if (i.currency_code === “GBP”) {
-  console.log(element.title, element.price);
-  }
+    if (i.currency_code === “GBP”) {
+      console.log(element.title, element.price);
+    }
   });
 }
 
@@ -44,32 +44,48 @@
 
 
 // 4: Display a list of all items who are made of wood.
-function question4 () {
-  // Answer:
+function question4()
+// Answer:
+
+let woodwork = []
+for (i = 0; i < data.length; i++) {
+  if (data[i].materials) {
+    let find = data[i].materials
+    for (j = 0; j < find.length; j++) {
+      if (find[j] === 'wood') {
+        woodwork.push(data[i].title)
+      }
+    }
+  }
+}
+console.log(woodwork)
 }
 
 
 // 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
-function question5 () {
+function question5() {
   // Answer:
-  element.materials.forEach(function(mat) {
-  if (mat === “wood”) {
-  console.log(element.title + ” is made of wood”);
-  }
-  });
-};
+  data.forEach(function(tree) {
+    if (tree.materials.length > 8) {
+      console.log(tree.title, tree.quantity, tree.materials.forEach(function(material) {
+        console.log(material)
+      }))
+    }
+  })
+}
 
 
 
 // 6: How many items were made by their sellers?
 // Answer:
-function question6 () {
+function question6() {
   // Answer:
-  data.forEach(function(element) {
-    if (element.materials.length >= :sunglasses: {
-      console.log(element.title, element.materials.length, element.materials);
-  }
-  });
-
+  let BySeller = 0
+  data.forEach(function(merchant) {
+    if (merchant.made === 'i_did') {
+      BySeller++
+    }
+  })
+  console.log(BySeller + " was made by merchant")
 }
